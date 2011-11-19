eb143191999d0c78be3e10cc2cd7e5fc8effad13
Sarah Subert <ThinkCobra@aol.com>
2011-11-19 16:15:31 +0100

    Initial commit

---
+++ spaethi_finder/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpaethiFinder::Application.config.secret_token = 'eda4fce95721782099d5d8515bf1c7f6f807445210b8f01c67d360ced481bb9752d6a94cdb96bad5b217e67163f7811872c8b45d15a7d0045751215e0600897c'
